4beca525d5b18738020f992e9df25c1d6419f35d
Ivan Kramnyuk <i.kramnyuk@gmail.com>
2021-12-14 19:27:05 +0200

    update

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-Install: 
-pip install flask numpy Pillow opencv-python
+## Installation
+1. Clone the repo
+```
 
-Run: 
-python -m flask run
+```
+2. Versions
+```
+Python - 3.9.0
+```
+
+3. Create a Python virtual environment named 'test' and activate it
+```
+$ pip install virtualenv
+$ virtualenv test
+```
+```
+$ source test/Scripts/activate
+```
+
+4. Now, run the following command in your Terminal/Command Prompt to install the libraries required
+```
+$ pip install -r requirements.txt
+```
+
+## Working
+
+1. Open terminal. Go into the cloned project directory and type the following command:
+```
+$ python train_mask_detector.py --dataset dataset
+```
+```
+$ python -m flask run
+```
--- app.py
+++ app.py
@@ -1,51 +1,64 @@
-from flask import Flask, render_template, request
-import numpy as np
+from flask import Flask,render_template,Response
+from flask_socketio import SocketIO, emit
 import cv2
-import base64
-from io import BytesIO
-from PIL import Image
 
+from detect_mask_video import detect_and_predict_mask
 
-app = Flask(__name__)
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+async_mode = None
+socket_ = SocketIO(app, async_mode=async_mode)
 
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        status = True
+        socket_.emit('update_status', {'status': status}, broadcast=True)
+        if not success:
+            break
+        else:
+            (locs, preds) = detect_and_predict_mask(frame)
+
+            # loop over the detected face locations and their corresponding
+            # locations
+            for (box, pred) in zip(locs, preds):
+                # unpack the bounding box and predictions
+                (startX, startY, endX, endY) = box
+                (mask, withoutMask) = pred
+
+                # determine the class label and color we'll use to draw
+                # the bounding box and text
+                status = mask > withoutMask
+                label = "Mask" if mask > withoutMask else "No Mask"
+                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+                
+                # include the probability in the label
+                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+                # display the label and bounding box rectangle on the output
+                # frame
+                cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', async_mode=socket_.async_mode)
 
-@app.route('/detection', methods=['POST'])
-def detection():
-    canvasdata = request.form['canvasimg']
-    encoded_data = request.form['canvasimg'].split(',')[1]
-    print(canvasdata[:50])
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    socket_.run(app, debug=True)
     
-    # Decode base64
-    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
-    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (30,30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (0,255,0), 2)
-    
-    cv2.imwrite('frame.jpg', frame)
-    
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    pil_img = Image.fromarray(frame)
-    buff = BytesIO()
-    pil_img.save(buff, format="JPEG")
-    base64_frame = base64.b64encode(buff.getvalue()).decode("utf-8")
-    base64_frame = 'data:image/png;base64,' + base64_frame
-
-    return render_template('index.html', canvasdata=base64_frame)
-
-if __name__ == '__main__':
-    app.run(debug = True)
